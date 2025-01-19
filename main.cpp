a281c00ba57acb6cc62e688e8b6d1e337772aaa7
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-20 01:29:12 +0200

    Finally got it working!

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ int main(void)
 
     std::cout << "Hello Terminal!" << std::endl;
 
+	if(!glfwInit) {
+		std::cout << "Something failed but, init system is working #rep +rep aki röps höps!" << std::endl;
+	} else {
+		std::cout << "Something is successfully wrong!" << std::endl;
+	}
+
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", glfwGetPrimaryMonitor(), NULL);
 
 	return 0;
