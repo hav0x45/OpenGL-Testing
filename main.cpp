c16215e4cc27cf3f7be1c41fbb07915f851d9d9f
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-20 12:25:31 +0200

    Some changes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #define GLFW_DLL
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <string>
 using namespace std;
 
-int main(void)
+int main()
 {
-
-    std::cout << "Hello Terminal!" << std::endl;
 
 	if(!glfwInit) {
 		std::cout << "Something failed succesfully!" << std::endl;
 	} else {
 		std::cout << "GLFW Successfully initialized!" << std::endl;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+		std::cout << "Something failed." << std::endl;
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
