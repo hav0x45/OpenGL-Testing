68217016754208f42d370a992c14f7cfabf77b1e
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-20 13:06:16 +0200

    Some changes

--- main.cpp
+++ main.cpp
@@ -7,21 +7,24 @@ using namespace std;
 int main()
 {
 
+	// Check for GLFW init
 	if(!glfwInit) {
 		std::cout << "Something failed succesfully!" << std::endl;
 	} else {
 		std::cout << "GLFW Successfully initialized!" << std::endl;
 	}
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Create a window
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     
+	// Check for the window
 	if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+	// Terminate the program
 	glfwTerminate();
 
 	return 0;
